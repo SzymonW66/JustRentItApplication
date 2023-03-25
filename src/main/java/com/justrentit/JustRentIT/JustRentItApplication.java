49f729877eac40bc4a3d32a0e9025513d8852dfe
SzymonW66 <szymekwajs@gmail.com>
2023-03-25 18:57:23 +0100

    initializing commit

---
+++ src/main/java/com/justrentit/JustRentIT/JustRentItApplication.java
@@ -0,0 +1,13 @@
+package com.justrentit.JustRentIT;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JustRentItApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JustRentItApplication.class, args);
+	}
+
+}
